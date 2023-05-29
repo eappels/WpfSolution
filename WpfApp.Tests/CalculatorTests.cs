22eaad77d8474b7b0c08f0bb627ced7fbdabb83a
appels@telenet.be <appels@telenet.be>
2023-05-29 18:45:21 +0200

    Add project files.

---
+++ WpfApp.Tests/CalculatorTests.cs
@@ -0,0 +1,64 @@
+﻿using Xunit;
+
+namespace WpfApp.Tests
+{
+    public class CalculatorTests
+    {
+
+        [Fact]
+        public void Add_SimpleValuesShouldCalculate()
+        {
+
+            // Arrange
+            int expected = 5;
+
+            // Act
+            int actual = Calculator.Add(3, 2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Substract_SimpleValuesShouldCalculate()
+        {
+
+            // Arrange
+            int expected = 5;
+
+            // Act
+            int actual = Calculator.Substract(8, 3);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Multiply_SimpleValuesShouldCalculate()
+        {
+
+            // Arrange
+            int expected = 15;
+
+            // Act
+            int actual = Calculator.Multiply(3, 5);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Divide_SimpleValuesShouldCalculate()
+        {
+
+            // Arrange
+            double expected = 3;
+
+            // Act
+            double actual = Calculator.Divide(9, 3);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ WpfApp/Calculator.cs
@@ -0,0 +1,27 @@
+﻿namespace WpfApp
+{
+
+    public static class Calculator
+    {
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Substract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
